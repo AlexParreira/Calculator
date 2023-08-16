d08a69d7c37781cd2f6fc995a3cd6fdf156055d5
AlexParreira <alexander.o.parreira@gmail.com>
2023-08-15 21:05:26 -0300

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Alexander Parreira on 15/08/23.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
